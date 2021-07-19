36f124ccce7e37ac45cedb28373f9099b037d82c
Theobald Beyer <theobald.beyer@in-tech.com>
2021-07-19 10:13:53 +0200

    WIP

---
+++ Examples/DwarfNameGenerator/DwarfUnit.cs
@@ -0,0 +1,23 @@
+﻿using SharpGrammar;
+using SharpGrammar.Memory;
+
+namespace DwarfNameGenerator
+{
+    public static class DwarfUnit
+    {
+        private const string Specialization = nameof(Specialization);
+        
+        public static readonly Processable<string> Unit = Take.OneOf();
+        
+        public static readonly Processable<string> SetSpecialization = 
+            Memory.Set(Specialization, Take.OneOf<UnitSpecialization>());
+    }
+
+    public enum UnitSpecialization
+    {
+        Ranged,
+        Melee,
+        Siege,
+        Scout
+    }
+}
