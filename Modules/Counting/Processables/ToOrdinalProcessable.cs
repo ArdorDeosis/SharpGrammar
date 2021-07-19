5a96bb57d13931adcb2f658ec3f386138f39732c
Theobald Beyer <theobald.beyer@in-tech.com>
2021-07-19 09:26:19 +0200

    added ToOrdinal conversion to number module

---
+++ Modules/Counting/Processables/ToOrdinalProcessable.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpGrammar.Counting
+{
+    internal record ToOrdinalProcessable : Processable<string>
+    {
+        private Processable<int> number;
+
+        public ToOrdinalProcessable(Processable<int> number)
+        {
+            this.number = number;
+        }
+
+        public override string Process(IContext context)
+        {
+            var n = number.Process(context);
+            return (n % 10) switch
+            {
+                1 => n + "st",
+                2 => n + "nd",
+                3 => n + "rd",
+                _ => n + "th"
+            };
+        }
+    }
+}
